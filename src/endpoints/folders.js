1c1da6bb7668bcdd9f7bba218ef179c988f51bf9
Max Presman <max@pubnub.com>
2017-01-05 19:01:32 -0800

    folder creation logic

--- src/endpoints/folders.js
+++ src/endpoints/folders.js
@@ -8,6 +8,47 @@ module.exports = class {
     this._config = config;
   }
 
+  create(createParams = {}) {
+    // create is just an update with id of 0.
+    createParams.id = 0;
+
+    return this.update(createParams);
+  }
+
+  update(updateParams = {}) {
+    return new Promise((resolve, reject) => {
+      const timeNow = new Date();
+      const currentMonth = timeNow.getMonth() + 1;
+      let timeNowString = `${currentMonth}/${timeNow.getDate()}/${timeNow.getFullYear()}`;
+      timeNowString += ` ${timeNow.getHours()}:${timeNow.getMinutes()}:${timeNow.getSeconds()}`;
+
+      const postBody = {
+        change_date: timeNowString,
+        id: updateParams.id,
+      };
+      const fields = [
+        'name',
+        'productId',
+        'divisionId',
+        'parentFolderId',
+      ];
+
+      fields.forEach((field) => {
+        if (_.has(updateParams, field)) {
+          postBody[_.snakeCase(field)] = updateParams[field];
+        }
+      });
+
+      const requestParams = networking.createBaseRequestParams(this._config, {
+        url: `ui/api/v1/folders/${updateParams.id}`,
+        postType: 'json',
+        body: postBody
+      });
+
+      return networking.post(requestParams, resolve, reject);
+    });
+  }
+
   getById({ id } = {}) {
     return new Promise((resolve, reject) => {
       if (!this._config.accessToken) return reject('missing access token');
